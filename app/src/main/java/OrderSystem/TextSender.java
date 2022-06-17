81d5613166da965d67b882898b37c4dd500f39b0
Theo <theo@TNT-DESKTOP.localdomain>
2022-06-17 17:03:09 +0100

    Added Twilio texting functionality

--- app/src/main/java/OrderSystem/TextSender.java
+++ app/src/main/java/OrderSystem/TextSender.java
@@ -4,16 +4,26 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-public class TextSender {
-  // Find your Account Sid and Token at twilio.com/user/account
-  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-  public static final String AUTH_TOKEN = "your_auth_token";
+import io.github.cdimascio.dotenv.Dotenv;
 
-  public static void sendMessage(String text, String mobile) {
+public class TextSender {
+  private final String ACCOUNT_SID;
+  private final String AUTH_TOKEN;
+  private final String FROM_NUMBER;
+
+  public TextSender() {
+    Dotenv dotenv = Dotenv.load();
+    this.ACCOUNT_SID = dotenv.get("ACCOUNT_SID");
+    this.AUTH_TOKEN = dotenv.get("AUTH_TOKEN");
+    this.FROM_NUMBER = dotenv.get("FROM_NUMBER");
+  }
+
+  public void sendMessage(String text, String mobile) {
     Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
-    Message message = Message.creator(new PhoneNumber(mobile),
-        new PhoneNumber("+15017250604"),
+    Message message = Message.creator(
+        new PhoneNumber(mobile),
+        new PhoneNumber(FROM_NUMBER),
         text).create();
 
     System.out.println(message.getSid());
