d21c089e612645195fbaa113a99814975486912f
Theo <theo@TNT-DESKTOP.localdomain>
2022-06-17 12:18:00 +0100

    Added Order and Text related classes and Customer

--- app/src/main/java/OrderSystem/OrderFormatter.java
+++ app/src/main/java/OrderSystem/OrderFormatter.java
@@ -1,5 +1,19 @@
 package OrderSystem;
 
-public class OrderFormatter {
+import java.time.format.DateTimeFormatter;
 
+public class OrderFormatter {
+  private Order order;
+
+  public OrderFormatter(Order order) {
+    this.order = order;
+  }
+
+  public void renderOrder() {
+    CartFormatter formatter = new CartFormatter(order.getCart());
+    DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("EEE dd-MM-yy HH:mm");
+
+    formatter.renderCart();
+    System.out.println(order.getOrderTime().format(timeFormatter));
+  }
 }
--- app/src/main/java/OrderSystem/TextSender.java
+++ app/src/main/java/OrderSystem/TextSender.java
@@ -1,5 +1,21 @@
 package OrderSystem;
 
-public class TextSender {
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 
+public class TextSender {
+  // Find your Account Sid and Token at twilio.com/user/account
+  public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+  public static final String AUTH_TOKEN = "your_auth_token";
+
+  public static void sendMessage(String text, String mobile) {
+    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+    Message message = Message.creator(new PhoneNumber(mobile),
+        new PhoneNumber("+15017250604"),
+        text).create();
+
+    System.out.println(message.getSid());
+  }
 }
